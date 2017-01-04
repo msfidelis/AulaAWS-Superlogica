77a2b6fa943b1bb047ce098fb811130915465324
Matheus Fidelis <msfidelis@outlook.com>
2017-01-04 13:19:12 -0200

    Primeiro Commit

---
+++ app/index.php
@@ -0,0 +1,32 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require './vendor/autoload.php';
+
+/**
+ * Default Index - Script de demonstração de Auto Scaling e Load Balance
+ * @var string
+ */
+$app->get('/', function (Request $request, Response $response) use ($app) {
+    $response->getBody()->write("<img src='https://credencial.imasters.com.br/media/imasters/parceiros/840_parceiro.png' />");
+    return $response;
+});
+
+/**
+ * Pega o hostname da máquina
+ */
+$app->get('/whoami', function (Request $request, Response $response) {
+    $response->getBody()->write(gethostname());
+    return $response;
+});
+
+/**
+ * Pega o IP da máquina
+ */
+$app->get('/cade', function (Request $request, Response $response) {
+    $response->getBody()->write(getHostByName(getHostName()));
+    return $response;
+});
+
+$app->run();
